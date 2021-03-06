2593a076584f1a48b9322f5cd7beb5a41f1d5110
Nikita <Nikita@TheVer4.ru>
2021-03-06 16:48:30 +0500

    Fully implementation of Triangle

--- Test.Logic/Triangle.cs
+++ Test.Logic/Triangle.cs
@@ -7,6 +7,17 @@ namespace Test.Logic {
         public Point b { get; set; }
         public Point c { get; set; }
 
+        public bool Exitst() {
+            int count = 0;
+            if (getLength(a, b) + getLength(b, c) > getLength(a, c))
+                count++;
+            if (getLength(a, b) + getLength(a, c) > getLength(b, c))
+                count++;
+            if (getLength(b, b) + getLength(a, c) > getLength(a, b))
+                count++;
+            return count == 3;
+        }
+        
         private int getLength(Point x, Point y) {
             return (int) Math.Sqrt(Math.Pow(x.X - y.X, 2) + Math.Pow(x.Y - y.Y, 2));
         }
