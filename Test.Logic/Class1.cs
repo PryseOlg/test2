917470cb9d12f887639c7311bfebc625f76d1754
Nikita <Nikita@TheVer4.ru>
2021-03-06 16:43:19 +0500

    Created Triangle.cs

--- Test.Logic/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace Test.Logic {
-    public class Class1 {
-    }
-}
---
+++ Test.Logic/Triangle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Drawing;
+
+namespace Test.Logic {
+    public class Triangle {
+        public Point a { get; set; }
+        public Point b { get; set; }
+        public Point c { get; set; }
+        
+    }
+}
